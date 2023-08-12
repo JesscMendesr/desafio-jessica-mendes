9bed64114cf08b00fcc709b66e36e3212789e386
Jessica Mendes <jessicamend17@gmail.com>
2023-08-12 18:32:14 -0300

    teste com apenas 1 erro

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,31 +1,33 @@
 const cardapio = {
     cafe: {
-        valor: 'R$ 3,00'
+        valor: 3.00,
+        tipo: 'principal'
     },
     chantily: {
-        valor: 'R$ 1,50',
+        valor: 1.50,
         extra: ''
     },
     suco: {
-        valor: 'R$ 6,20'
+        valor: 6.20
     }, 
     sanduiche: {
-        valor: 'R$ 6,50'
+        valor: 6.50
     },
     queijo: {
-        valor: 'R$ 2,00', extra: ''
+        valor: 2.00, 
+        extra: ''
     },
     salgado: {
-        valor: 'R$ 7,25'
+        valor: 7.25
     },
     combo1: {
-        valor: 'R$ 9,50'
+        valor: 9.50
     },
     combo2: {
-        valor: 'R$ 7,50'
+        valor: 7.50
     }
 }
-
+// Função para calcular valor da taxa ou desconto, caso houver.
 function calcularDescontoTaxa(metodoDePagamento, valorTotal){
     let precoFinal;
     if(metodoDePagamento === 'dinheiro'){
@@ -36,57 +38,108 @@ function calcularDescontoTaxa(metodoDePagamento, valorTotal){
         let acrescimo;
         acrescimo = valorTotal * (3 / 100)
         precoFinal = valorTotal + acrescimo
+    }else{
+        precoFinal = valorTotal
     }
     return precoFinal.toFixed(2)
 }
 
-console.log(calcularDescontoTaxa('credito', 5.10))
 
+
+// Função para verificar se existe algum item extra.
 function verificarItemExtra(item){
     return cardapio[item].hasOwnProperty('extra')
 }
 
-verificarItemExtra('chantily')
+// Função para validar o método de pagamento
+function validarFormaDePagamento(metodoDePagamento){
+    const pagamentosAceitos = ['dinheiro', 'credito', 'debito']
+    return pagamentosAceitos.includes(metodoDePagamento)
+}
+
+// Função para verificar se existe item principal na lista de Itens.
+function validarItemPrincipal(listaDeItens, itemExtra){
+    let count = 0
+
+    if(itemExtra === 'chantily'){
+        listaDeItens.forEach(item =>{
+            if(item[0] === 'cafe'){
+                count++
+            }
+        })
+    }else if (itemExtra === 'queijo'){
+        listaDeItens.forEach(item =>{
+            if(item[0] === 'sanduiche'){
+                count++
+            }
+        })
+    }
+
+    return count > 0
+}
+
+
+//console.log(validarItemPrincipal([ [ 'chantily', 1 ], [ 'sanduiche', 1 ] ], 'chantily'))
+
+
+function calcularValorFinal(listaDeItens){
+    let valorFinal = 0
+    listaDeItens.forEach(item => {
+    let nomeItem = item[0]
+    valorFinal += parseFloat((cardapio[nomeItem].valor) * item[1])
+    })
+    return parseFloat(valorFinal.toFixed(2))
+}
+
 
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         let resposta;
-        const pagamentosAceitos = ['dinheiro', 'credito', 'debito']
+        let novaLista = []
 
-        if(pagamentosAceitos.hasOwnProperty(metodoDePagamento)){
+        
+        if(validarFormaDePagamento(metodoDePagamento)){
             if(itens.length !== 0){
                 itens.forEach(element => {
                     let item = element.slice(0,element.length - 2)
                     let quantidade = parseInt(element.charAt(element.length - 1))
+                    novaLista.push([item, quantidade])
                     if(quantidade <= 0){
                         resposta = "Quantidade inválida!"
                     }else if((cardapio.hasOwnProperty(item)) === false){
                         resposta = "Item inválido!"
                     }else{
-
+                        if(verificarItemExtra(item)){
+                            console.log(item)
+                            if(validarItemPrincipal(novaLista, item)){
+                                let valorTotal = calcularDescontoTaxa(metodoDePagamento, calcularValorFinal(novaLista))
+                                resposta = `R$ ${valorTotal.toString().replace('.', ',')}`
+                            }else{
+                                resposta = "Item extra não pode ser pedido sem o principal"
+                            }
+                        }else{
+                            let valorTotal = calcularDescontoTaxa(metodoDePagamento, calcularValorFinal(novaLista))
+                            resposta = `R$ ${valorTotal.toString().replace('.', ',')}`
+                            
+                        }
                     }
-                    console.log(cardapio.hasOwnProperty(item))
-                    console.log(quantidade)
                 });
-        
             }else{
                 resposta = "Não há itens no carrinho de compra!";
             }
-
         }else{
             resposta = "Forma de pagamento inválida!"
         }
-
-        return '';
+        return resposta;
     }
     
 }
 
 const resultado = new CaixaDaLanchonete()
-            .calcularValorDaCompra('dinheiro', ['cafe,5','chantily,1']
-            );
+               .calcularValorDaCompra('debito', ['chantily,1', 'sanduiche,1']); // 'R$ 33,73'
 
 console.log(resultado)    
+
 
 export { CaixaDaLanchonete };
